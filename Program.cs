1747a547a23be369bdc707dd20e4548375a9011e
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-05-28 14:43:22 -0500

    Added words and definitions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dictionary_of_words
 {
@@ -6,7 +7,33 @@ namespace dictionary_of_words
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* Create a dictionary with key value pairs to represent words (key) and its definition (value)*/
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+
+            // Add several more words and their definitions
+            wordsAndDefinitions.Add("blandishment", "flattery intended to persuade");
+            wordsAndDefinitions.Add("conflagration", "a very intense and uncontrolled fire");
+            wordsAndDefinitions.Add("diaphanous", "so thin as to transmit light");
+            wordsAndDefinitions.Add("ephemeral", "anything short-lived");
+            wordsAndDefinitions.Add("fortuitous", "lucky or occurring by happy chance");
+            wordsAndDefinitions.Add("grandiloquent", "lofty in style");
+            wordsAndDefinitions.Add("intimation", "a slight suggestion or vague understanding");
+            wordsAndDefinitions.Add("multifarious", "having many aspects");
+
+            /*
+                Use square bracket lookup to get the definition two
+                words and output them to the console
+            */
+
+            /*
+                Loop over dictionary to get the following output:
+                    The definition of [WORD] is [DEFINITION]
+                    The definition of [WORD] is [DEFINITION]
+                    The definition of [WORD] is [DEFINITION]
+            */
+            foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+            {
+            }
         }
     }
 }
